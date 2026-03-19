0947b90951d8e6446c5621b5309b4047260fcab1
vj-004 <vj20190204@gmail.com>
2026-03-19 21:54:58 +0530

    missed to put https in last commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
     cors({
-        origin: ["http://localhost:3000", "study-notion-client-nu.vercel.app"],
+        origin: ["http://localhost:3000", "https://study-notion-client-nu.vercel.app"],
         credentials: true,
     })
 );
